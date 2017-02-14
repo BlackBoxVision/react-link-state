ea0c4b4a443a3145fd52eb6d5c01a4ce6f6d2028
jonatansalas <jonatansalas@live.com.ar>
2017-02-14 16:17:57 -0300

    Fixed test case Added chai to devDependencies

--- test/withLinkState.spec.js
+++ test/withLinkState.spec.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
+import { expect } from 'chai';
 
 import withLinkState from '../src/lib/withLinkState';
 
 //Simple component to test behavior
-const Component = (props) => (
+const Component = props => (
     <div>
         <input name="test" {...props.linkState('test')}/>
         <p>{props.getValue('test')}</p>
     </div>
-)
+);
 
-describe('<withLinkState/>' () => {
+describe('<withLinkState/>', () => {
     it('<withLinkState/> instance is not null', () => {
-        const WrappedComponent = shallow(withLinkState(Component));
-        expect(WrappedComponent).to.not.equal('null')
+        //Create an element by passing Component class wrapped
+        const wrappedComponent = React.createElement(withLinkState(Component));
+        const renderedComponent = shallow(wrappedComponent);
+
+        expect(renderedComponent).to.not.equal('null');
     })
 });
