19d3eefe78d411db35ca42a3ec892d484b40c37a
jonatansalas <jonatansalas@live.com.ar>
2017-02-14 17:22:05 -0300

    Added more case under test

--- test/withLinkState.spec.js
+++ test/withLinkState.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import { shallow } from 'enzyme';
 
 import withLinkState from '../src/lib/withLinkState';
 
@@ -12,12 +12,68 @@ const Component = props => (
     </div>
 );
 
-describe('<withLinkState/>', () => {
-    it('<withLinkState/> instance is not null', () => {
+describe('Testing -> <LinkStateComponent/>', () => {
+    it('instance of <LinkStateComponent/> is not undefined', () => {
+        //Create an element by passing Component class wrapped
+        const wrappedComponent = React.createElement(withLinkState(Component));
+        const renderedComponent = shallow(wrappedComponent);
+
+        expect(renderedComponent).to.not.equal('undefined');
+    });
+
+    it('instance of <Component/> wrapped in <LinkStateComponent/> is not undefined', () => {
+        //Create an element by passing Component class wrapped
+        const wrappedComponent = React.createElement(withLinkState(Component));
+        const renderedComponent = shallow(wrappedComponent);
+
+        expect(renderedComponent.find(Component)).to.not.equal('undefined');
+    });
+
+    it('instance of <LinkStateComponent/> is not null', () => {
         //Create an element by passing Component class wrapped
         const wrappedComponent = React.createElement(withLinkState(Component));
         const renderedComponent = shallow(wrappedComponent);
 
         expect(renderedComponent).to.not.equal('null');
+    });
+
+    it('instance of <Component/> wrapped in <LinkStateComponent/> is not null', () => {
+        //Create an element by passing Component class wrapped
+        const wrappedComponent = React.createElement(withLinkState(Component));
+        const renderedComponent = shallow(wrappedComponent);
+
+        expect(renderedComponent.find(Component)).to.not.equal('null');
+    });
+
+    it('linkState from <LinkStateComponent/> is not undefined', () => {
+        //Create an element by passing Component class wrapped
+        const wrappedComponent = React.createElement(withLinkState(Component));
+        const renderedComponent = shallow(wrappedComponent);
+
+        expect(renderedComponent.find('linkState')).to.not.equal('undefined');
+    });
+
+    it('getValue from <LinkStateComponent/> is not undefined', () => {
+        //Create an element by passing Component class wrapped
+        const wrappedComponent = React.createElement(withLinkState(Component));
+        const renderedComponent = shallow(wrappedComponent);
+
+        expect(renderedComponent.find('getValue')).to.not.equal('undefined');
+    })
+
+    it('linkState from <LinkStateComponent/> is not null', () => {
+        //Create an element by passing Component class wrapped
+        const wrappedComponent = React.createElement(withLinkState(Component));
+        const renderedComponent = shallow(wrappedComponent);
+
+        expect(renderedComponent.find('linkState')).to.not.equal('null');
+    });
+
+    it('getValue from <LinkStateComponent/> is not null', () => {
+        //Create an element by passing Component class wrapped
+        const wrappedComponent = React.createElement(withLinkState(Component));
+        const renderedComponent = shallow(wrappedComponent);
+
+        expect(renderedComponent.find('getValue')).to.not.equal('null');
     })
 });
